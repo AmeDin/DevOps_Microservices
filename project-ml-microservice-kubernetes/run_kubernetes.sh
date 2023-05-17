04722835dc49b4957a457da532040532b309a9c5
AmeDin <weeb.amiruddin@gmail.com>
2023-05-17 12:24:32 +0000

    add docker out predict

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="amedin/microproj:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microproj --image=amedin/microproj:v1.0.0 --port=80 --labels app=microproj
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward microproject 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="amedin/microproj:v1.0.0"
+
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag microproj $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
